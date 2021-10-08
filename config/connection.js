9f202e73d90cd3f6cb9cd34465875803ac0c9d10
Rachel <rtc145@gmail.com>
2021-10-08 10:01:05 -0400

    deploying

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost/workout', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+connect(
+  process.env.MONGO_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
 
 module.exports = connection;

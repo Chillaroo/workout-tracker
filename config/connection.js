6d00a0d9c63cf892cf7539e9cf05ed2fbd840c61
Rachel <rtc145@gmail.com>
2021-10-08 11:51:39 -0400

    updating database connection

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const { connect, connection } = require('mongoose');
 
 connect(  
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wxoba.mongodb.net/workout?retryWrites=true&w=majority`,
-  // process.env.MONGO_URI || 'mongodb://localhost/workout',
+  // `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wxoba.mongodb.net/workout?retryWrites=true&w=majority`,
+   process.env.MONGO_URI || 'mongodb://localhost/workout',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
